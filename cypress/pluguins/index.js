13cd7cc52624727c3ec763f940c7c253a378dbe2
icunha <israel.cunha@mc1global.com>
2022-10-03 11:54:58 -0300

    Add git ignore

---
+++ cypress/pluguins/index.js
@@ -0,0 +1,7 @@
+mudules.exports = (on, config) => {
+}
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+}
